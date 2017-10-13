90e55e875145d6ad1de7e393497b2e20d730719b
Brian McMahon <cipher813@gmail.com>
2017-10-13 16:22:57 +0630

    url edits, code in view and tests

--- lists/views.py
+++ lists/views.py
@@ -3,24 +3,13 @@ from lists.models import Item
 # from django.http import HttpResponse
 
 # Create your views here.
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
+
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
     return render(request,'home.html')
 
 def view_list(request):
     items = Item.objects.all()
     return render(request,'list.html', {'items': items})
-
-    #     new_item_text = request.POST['item_text']
-    #     Item.objects.create(text=new_item_text)
-    # else:
-    #     new_item_text = ''
-    #
-    # return render(request,'home.html',{
-    #     'new_item_text': new_item_text,
-# })
-    # if request.method == 'POST':
-    #     return HttpResponse(request.POST['item_text'])
-    # return render(request,'home.html')
