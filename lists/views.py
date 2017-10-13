24015a6373428827aae5e06e42579e8187e57c55
Brian McMahon <cipher813@gmail.com>
2017-10-13 16:56:17 +0630

    migrations, changed files

--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 # from django.http import HttpResponse
 
 # Create your views here.
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
 
 def home_page(request):
